3c8f483a8b948ee49a531492b645c22ed59bb956
Sean Bae <seanbae115@gmail.com>
2018-04-21 17:44:20 -0700

    db updated

--- app/config/config.php
+++ app/config/config.php
@@ -2,13 +2,13 @@
     // DB Params
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
-    define('DB_PASS', 'root');
+    define('DB_PASS', 'Akdldptmzbdpf1217');
     define('DB_NAME', 'shareposts');
 
     // App Root
     define('APPROOT', dirname(dirname(__FILE__)));
     // URL Root
-    define('URLROOT', 'http://localhost:8888/shareposts');
+    define('URLROOT', 'http://shareposts.bae-sw.us');
     // Site Name
     define('SITENAME', 'SharePosts');
     
